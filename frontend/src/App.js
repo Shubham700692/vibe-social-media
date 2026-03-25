e6701a796965807b79f6caf345c8a82b85d89847
Shubham700692 <shubhamkhanzode151@gmail.com>
2026-03-25 23:25:57 +0530

    fix build error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./theme";
 import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Feed from "./pages/Feed";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -14,14 +13,13 @@ const App = () => (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          {/* Public routes */}
+          
           <Route path="/login"  element={<Login />} />
           <Route path="/signup" element={<Signup />} />
 
-          {/* Feed is public (anyone can view), but CreatePost is gated inside Feed */}
+          
           <Route path="/" element={<Feed />} />
 
-          {/* Catch-all */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
